d201d60a23adf86dcc30c0dfdb1242c824155769
Siddharth Patel <sid.auburn@gmail.com>
2022-01-06 11:23:55 -0500

    add user auth and fix migration issue

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,16 @@ Rails.application.routes.draw do
     resources :sales, except: [:edit, :update, :show]
   end
 
+  # User authentication routes.
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
