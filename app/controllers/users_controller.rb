e2f00fcad373b1e65c6acfaa4ffb893e8dfd34d2
Siddharth Patel <sid.auburn@gmail.com>
2022-01-10 14:53:24 -0500

    added tests for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = user.id
+      session[:user_id] = @user.id
       redirect_to '/'
     else
       redirect_to '/signup'
@@ -12,6 +12,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:first_name, :last_name, :email.downcase.strip, :password, :password_confirmation)
   end
 end
