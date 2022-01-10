f670dfe8a4d82742f01a3b3786bc18502d235373
Siddharth Patel <sid.auburn@gmail.com>
2022-01-10 12:05:07 -0500

    working on user tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def create
-    user = User.new(user_params)
-    if user.save
+    @user = User.new(user_params)
+    if @user.save
       session[:user_id] = user.id
       redirect_to '/'
     else
